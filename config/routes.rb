f0021c362032d607acb9bcd21c62a76f8caabd37
Philip Ingram <philip.d.ingram@gmail.com>
2015-02-08 09:59:47 -0800

    add responses controller and view

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,10 @@
 Rails.application.routes.draw do
+  get 'responses/index'
+
   root to: 'static_pages#home'
 
   get 'questions/:id',    to: 'questions#show'
   get 'questions/search', to: 'questions#search'
+
+  get 'questions/:id/responses', to: 'responses#index'
 end
